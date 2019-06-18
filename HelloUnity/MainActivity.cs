c7fda976fe201e0103ef9e5937115d3b36243d9e
EgorBo <egorbo@gmail.com>
2019-06-18 12:44:22 +0300

    Initial commit

---
+++ HelloUnity/MainActivity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace HelloUnity
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            var fab = FindViewById<Button>(Resource.Id.openUnityBtn);
+            fab.Click += (s, e) =>
+            {
+                // open fullscreen unity activity
+                StartActivity(typeof(UnityActivity));
+            };
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+	}
+}
+
---
+++ HelloUnity/UnityActivity.cs
@@ -0,0 +1,62 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Widget;
+using Com.Company.Product;
+
+namespace HelloUnity
+{
+    [Activity(Label = "Hey!", 
+        ScreenOrientation = ScreenOrientation.FullSensor, 
+        LaunchMode = LaunchMode.SingleTask,
+        HardwareAccelerated = true, 
+        ConfigurationChanges = ConfigChanges.Mcc | 
+                               ConfigChanges.Mnc | 
+                               ConfigChanges.Locale |
+                               ConfigChanges.UiMode |
+                               ConfigChanges.Density |
+                               ConfigChanges.Keyboard |
+                               ConfigChanges.FontScale |
+                               ConfigChanges.ScreenSize |
+                               ConfigChanges.Navigation |
+                               ConfigChanges.Touchscreen | 
+                               ConfigChanges.Orientation | 
+                               ConfigChanges.ScreenLayout | 
+                               ConfigChanges.KeyboardHidden |
+                               ConfigChanges.LayoutDirection |
+                               ConfigChanges.SmallestScreenSize)]
+    public class UnityActivity : OverrideUnityActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Button myButton = new Button(this);
+            myButton.Text = "Xamarin button 1!";
+            myButton.SetX(10);
+            myButton.SetY(500);
+            myButton.Click += (s, e) =>
+            {
+                UnitySendMessage("Cube", "ChangeColor", "yellow");
+            };
+
+            UnityFrameLayout.AddView(myButton, 400, 200);
+
+            Button myButton2 = new Button(this);
+            myButton2.Text = "Xamarin button 2!";
+            myButton2.SetX(410);
+            myButton2.SetY(500);
+            myButton2.Click += (s, e) =>
+            {
+                UnitySendMessage("Cube", "ChangeColor", "red");
+            };
+
+            UnityFrameLayout.AddView(myButton2, 400, 200);
+        }
+
+        protected override void ShowMainActivity(string p)
+        {
+        }
+    }
+}
